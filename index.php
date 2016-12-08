39cf3152689904d020aacee64cf74c640cdc68cc
Rhys Packer <rhys.pacs@yahoo.com>
2016-12-08 05:00:42 +0000

    Hero added - with particle.js
    Particle.js added and optimised with image added in background.

--- index.php
+++ index.php
@@ -11,12 +11,17 @@ $cssPath = 'css/styles.css';
 include('includes/header.php'); 
 
 ?>
+      
+<!-- particles.js container -->
+  <div id="particles-js"></div>
 
-      
-      
+<!-- particles.js lib (JavaScript CodePen settings): https://github.com/VincentGarreau/particles.js -->
+  <script src='http://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js'></script>
+  <script src="js/particles.js"></script>
    
+
+
 
 <?php include('includes/footer.php'); ?>
 
-  </body>
-</html>
+ 
