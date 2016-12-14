ded86690aa59834819d116e025ee78fda7e43f27
Rhys Packer <rhys.pacs@yahoo.com>
2016-12-14 03:36:04 +0000

    Added gulp, added more scss for main, footer, layout and font variables
    Gulp is not control sass, prepros is. Gulp will hopefully be used for
    Svg Sprites.

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,27 @@
 
 
 
-<footer>
+<footer container="flex-container" role="contentinfo">
+
+	<div class="flex-item" >
+
+		<div class="social">
+
+			<a href="https://github.com/Rhyspacker/rhyspacker.github.io" title="External link to Rhys's GitHub page"> 
+				<img src="icons/git.png" alt="">
+			</a>
+
+			<a href="https://twitter.com/Rhyspacker" title="External link to Rhys's Twitter profile"> 
+				<img src="icons/twitter.png" alt="">
+			</a>
+
+			<a href="https://uk.linkedin.com/pub/rhys-packer/aa/b99/996" title="External link to Rhys's LinkedIn profilee"> 
+				<img src="icons/linkedin.png" alt="">
+			</a>
+
+		</div>
+
+	</div>
 
 </footer>
 
