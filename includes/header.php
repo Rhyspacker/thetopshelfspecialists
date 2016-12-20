ee39803714ddab15724b35bd5972a32139be9119
Rhys Packer <rhys.pacs@yahoo.com>
2016-12-20 05:34:12 +0000

    More crucial metadata added to header template file

--- includes/header.php
+++ includes/header.php
@@ -6,8 +6,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no"> 
     <meta name="description" content="<?php echo $pageDescription; ?>">
     <meta name="keywords" content="<?php echo $pageKeywords; ?>">
-    
+    <meta name="author" content="The Top Shelf Specialists">
+
+    <meta property="og:title" content="<?php echo $pageTitle; ?>" >
+    <meta property="og:description" content="<?php echo $pageDescription; ?>" >
+    <meta property="og:type" content="website" >
+    <meta property="og:url" content="<?php echo $pageUrl; ?>" >
+    <meta property="og:site_name" content="The Top Shelf Specialists | Manchester Bar Consultancy" >
+
     <link href="<?php echo $cssPath; ?>" type="text/css" rel="stylesheet"/>
+
+
   </head>
   
 
