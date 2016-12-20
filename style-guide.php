a5e4a57758d39d95488ef6c6711d1370b7a63ba9
Rhys Packer <rhys.pacs@yahoo.com>
2016-12-20 07:53:31 +0000

    Style guide created to start designing ui elements
    Colours, headings and buttons added to style-guide.php. Will add other
    ui elements later. Style guide will be removed on deployment, used for
    design stage only.

---
+++ style-guide.php
@@ -0,0 +1,134 @@
+
+
+
+<?php
+// Define variables for SEO
+$pageTitle = 'Home | The Top Shelf Specialists';
+$pageDescription = 'We are an award winning bar consultancy agency based in Manchester...';
+$pageKeywords = 'Manchester, Bar, Bar consultancy, cocktails, top shelf, the top shelf specialists';
+$pageUrl = 'http://thetopshelfspecialists.co.uk';
+
+$cssPath = 'css/styles.css';
+
+include('includes/header.php'); 
+
+?>
+
+
+<main>
+
+		<h1 style="font-weight: bold; text-decoration: underline;">COLOURS</h1>
+
+		<h3>$mainBg</h3>
+
+		<div id="mainBg" class="colourBox">
+		</div>
+
+		<br>
+
+		<h3>$secondBg</h3>
+
+		<div id="secondBg" class="colourBox">
+		</div>
+
+		<br>
+
+		<h3>$hoverColour</h3>
+
+		<div id="hoverColour" class="colourBox">
+		</div>
+
+		<br>
+
+		<h3>$linkColour</h3>
+
+		<div id="linkColour" class="colourBox">
+		</div>
+
+		<br>
+
+		<h3>$linkColourHover</h3>
+
+		<div id="linkColourHover" class="colourBox">
+		</div>
+
+		<br>
+
+		<h1 style="font-weight: bold; text-decoration: underline;">HEADINGS</h1>
+
+		<br>
+
+		<h1>Heading h1</h1>
+		<p>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation.
+		</p>
+
+		<br>
+
+		<h2>Heading h1</h2>
+		<p>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation.
+		</p>
+
+		<br>
+
+		<h3>Heading h1</h3>
+		<p>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation.
+		</p>
+
+		<br>
+
+		<h4>Heading h1</h4>
+		<p>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation.
+		</p>
+
+		<br>
+
+		<h5>Heading h1</h5>
+		<p>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation.
+		</p>
+
+		<br>
+
+		<h1 style="font-weight: bold; text-decoration: underline;">BUTTONS</h1>
+
+		<br>
+
+		<h3>Standard Button</h3>
+
+		<button>
+			BUTTON
+		</button>
+
+		<br>
+
+		<h3>Input Type Submit</h3>
+
+		<input type="submit" value="SUBMIT" title="submit your message" />
+
+		<br>
+
+		<h3>Call to Action Button</h3>
+
+		<button class="calltoAction">
+			BUTTON
+		</button>
+
+</main>
+
+<?php include('includes/footer.php'); ?>
+
+ 
