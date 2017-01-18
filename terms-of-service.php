ce8bfa10768b619a460615292a7c161bcc0041b8
Rhys Packer <rhys.pacs@yahoo.com>
2017-01-18 00:08:04 +0000

    Legal pages added - links in footer

---
+++ terms-of-service.php
@@ -0,0 +1,77 @@
+
+
+
+<?php
+// Define variables for SEO
+$pageTitle = 'Terms of Service | The Top Shelf Specialists';
+$pageDescription = 'Terms of service for The Top Shelf Specialists';
+$pageKeywords = 'terms, terms of service, terms and conditions, the top shelf specialists';
+$pageUrl = 'http://thetopshelfspecialists.co.uk/terms-of-service';
+
+$cssPath = 'css/styles.css';
+
+include('includes/header.php');
+
+?>
+
+<div class="pageheader" role="heading">
+
+	<p><a href="index.php">HOME</a> / TERMS OF SERVICE</p>
+	<h1>TERMS OF SERVICE</h1>
+
+</div>
+
+<main>
+
+			<h2>1. Terms</h2>
+
+			  <p>By accessing the website at <a href="http://thetopshelfspecialists.co.uk/">http://thetopshelfspecialists.co.uk/</a>, you are agreeing to be bound by these terms of service, all applicable laws and regulations, and agree that you are responsible for compliance with any applicable local laws. If you do not agree with any of these terms, you are prohibited from using or accessing this site. The materials contained in this website are protected by applicable copyright and trademark law.</p>
+
+			<h2>2. Use License</h2>
+
+			  <ol type="a">
+			    <li>
+			      Permission is granted to temporarily download one copy of the materials (information or software) on The Top Shelf Specialists's website for personal, non-commercial transitory viewing only. This is the grant of a license, not a transfer of title, and under this license you may not:
+
+			      <ol type="i">
+			        <li>modify or copy the materials;</li>
+			        <li>use the materials for any commercial purpose, or for any public display (commercial or non-commercial);</li>
+			        <li>attempt to decompile or reverse engineer any software contained on The Top Shelf Specialists's website;</li>
+			        <li>remove any copyright or other proprietary notations from the materials; or</li>
+			        <li>transfer the materials to another person or "mirror" the materials on any other server.</li>
+			      </ol>
+			    </li>
+			    <li>This license shall automatically terminate if you violate any of these restrictions and may be terminated by The Top Shelf Specialists at any time. Upon terminating your viewing of these materials or upon the termination of this license, you must destroy any downloaded materials in your possession whether in electronic or printed format.</li>
+			  </ol>
+
+			<h2>3. Disclaimer</h2>
+
+			  <ol type="a">
+			    <li>The materials on The Top Shelf Specialists's website are provided on an 'as is' basis. The Top Shelf Specialists makes no warranties, expressed or implied, and hereby disclaims and negates all other warranties including, without limitation, implied warranties or conditions of merchantability, fitness for a particular purpose, or non-infringement of intellectual property or other violation of rights.</li>
+			    <li>Further, The Top Shelf Specialists does not warrant or make any representations concerning the accuracy, likely results, or reliability of the use of the materials on its website or otherwise relating to such materials or on any sites linked to this site.</li>
+			  </ol>
+
+			<h2>4. Limitations</h2>
+
+			  <p>In no event shall The Top Shelf Specialists or its suppliers be liable for any damages (including, without limitation, damages for loss of data or profit, or due to business interruption) arising out of the use or inability to use the materials on The Top Shelf Specialists's website, even if The Top Shelf Specialists or a The Top Shelf Specialists authorized representative has been notified orally or in writing of the possibility of such damage. Because some jurisdictions do not allow limitations on implied warranties, or limitations of liability for consequential or incidental damages, these limitations may not apply to you.</p>
+
+			<h2>5. Accuracy of materials</h2>
+
+			  <p>The materials appearing on The Top Shelf Specialists's website could include technical, typographical, or photographic errors. The Top Shelf Specialists does not warrant that any of the materials on its website are accurate, complete or current. The Top Shelf Specialists may make changes to the materials contained on its website at any time without notice. However The Top Shelf Specialists does not make any commitment to update the materials.</p>
+
+			<h2>6. Links</h2>
+
+			  <p>The Top Shelf Specialists has not reviewed all of the sites linked to its website and is not responsible for the contents of any such linked site. The inclusion of any link does not imply endorsement by The Top Shelf Specialists of the site. Use of any such linked website is at the user's own risk.</p>
+
+			<h2>7. Modifications</h2>
+
+			  <p>The Top Shelf Specialists may revise these terms of service for its website at any time without notice. By using this website you are agreeing to be bound by the then current version of these terms of service.</p>
+
+			<h2>8. Governing Law</h2>
+
+			  <p>These terms and conditions are governed by and construed in accordance with the laws of United Kingdom and you irrevocably submit to the exclusive jurisdiction of the courts in that State or location.</p>
+
+
+</main>
+
+<?php include('includes/footer.php'); ?>
